de8198b2b5c2e9277f228bffa0ed9b0e41fee039
Brad-Galindo <mduck9419@gmail.com>
2024-06-03 20:36:56 -0500

    some chatgnes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://starter-name-generator-its-also.onrender.com/')
+    fetch('https://starter-name-generator-its-unique.onrender.com/')
       .then((res) => res.json())
       .then((nme) => setName([nme]))
   }, [])
